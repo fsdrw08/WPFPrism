7756fdcccefbfadab0eeeb29f43b4bf3a5333639
Windom WU <drw_08@hotmail.com>
2020-07-24 11:32:20 +0800

    Add project files.

---
+++ WPFPrism/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WPFPrism.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
